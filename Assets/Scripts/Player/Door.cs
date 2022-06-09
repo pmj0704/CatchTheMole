861aa400150542103b85ba304c776fb487994a74
pmj0704 <ggm.s210012@ggh.goe.go.kr>
2022-06-10 00:19:37 +0900

    Player Cam Change When Indoors

--- Assets/Scripts/Player/Door.cs
+++ Assets/Scripts/Player/Door.cs
@@ -8,18 +8,23 @@ public class Door : MonoBehaviour, InteractiveObj
 
     bool on = false;
 
+    private void Start()
+    {
+        DOTween.Init(false, false, LogBehaviour.Default).SetCapacity(100, 20);
+    }
+
     public void Interact()
     {
         if(on)
         {
-            Vector3 open = new Vector3(-90.0f, 0f, 90);
-            transform.DORotate(open, 3f);
+            Vector3 open = new Vector3(0f, 0f, 90);
+            transform.DORotate(open, 1.0f, RotateMode.LocalAxisAdd);
             on = false;
         }
         else
         {
-            Vector3 close = new Vector3(-90.0f, 0f, 180);
-            transform.DORotate(close, 3f);
+            Vector3 close = new Vector3(0f, 0f, -90);
+            transform.DORotate(close, 1.0f, RotateMode.LocalAxisAdd);
             on = true;
         }
     }
---
+++ Assets/Scripts/Player/InHouse.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InHouse : MonoBehaviour
+{
+    public Transform camView;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Vector3 newPos = new Vector3(0,1,0);
+            camView.localPosition = newPos;
+            Camera.main.GetComponent<CameraCtrl>().distance = 1.5f;
+            Camera.main.GetComponent<CameraCtrl>().height =  0.5f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Vector3 newPos = new Vector3(0, 2, 0);
+            camView.localPosition = newPos;
+            Camera.main.GetComponent<CameraCtrl>().distance = 6.0f;
+            Camera.main.GetComponent<CameraCtrl>().height = 2.0f;
+        }
+    }
+}
